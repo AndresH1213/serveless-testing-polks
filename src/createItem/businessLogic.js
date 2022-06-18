74a2d9921887807000fbc6825098be41896b56d2
Andres Hernandez <edanhebla1213@hotmail.com>
2022-06-17 22:00:50 -0500

    fixed 109239

--- src/createItem/businessLogic.js
+++ src/createItem/businessLogic.js
@@ -1,24 +1,24 @@
-const log = require('serverless-logger')(__filename)
+const log = require('serverless-logger')(__filename);
 
 const performCalculation = ({ a, b, method }) => {
-  log(`Received method="${method}" and values: a=${a}; b=${b}`)
+  log(`Received method="${method}" and values: a=${a}; b=${b}`);
   switch (method) {
     case 'add':
-      return a + b
-    // To Do - implement other methods (just sample, not a real to do)
+      return a + b;
+    // To Do - implement other methods
     default:
-      throw new NotImplementedYetError()
+      throw new NotImplementedYetError();
   }
-}
+};
 
 class NotImplementedYetError extends Error {
   constructor() {
-    super()
-    this.status = 400
-    this.statusCode = 400
-    this.name = 'NotImplementedYetError'
-    this.message = 'Not implemented yet!'
+    super();
+    this.status = 400;
+    this.statusCode = 400;
+    this.name = 'NotImplementedYetError';
+    this.message = 'Not implemented yet!';
   }
 }
 
-module.exports = { performCalculation }
+module.exports = { performCalculation };
